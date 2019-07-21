ce4495d2d2ca85ad9a9a05c0db695fc600e51328
Gerald Aburto Urroz <aburtogerald@gmail.com>
2019-07-20 21:13:37 -0600

    Updates

---
+++ precache-manifest.3964f52bf2feaa09ecf68e3e2c4f3c77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5f40106cf243cad059caae05d4526b",
+    "url": "/codefriends/index.html"
+  },
+  {
+    "revision": "0dfe9aa811ba110216a3",
+    "url": "/codefriends/static/css/main.f74d2d2e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/codefriends/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0dfe9aa811ba110216a3",
+    "url": "/codefriends/static/js/main.cd4bafaf.chunk.js"
+  },
+  {
+    "revision": "6b57255493c17bb18970",
+    "url": "/codefriends/static/js/runtime~main.1dab4f5b.js"
+  }
+]);
